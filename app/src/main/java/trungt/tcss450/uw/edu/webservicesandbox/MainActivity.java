cd5268cb764c1c41a33e17cfb6e0034d34264826
trungt19 <Trtt06070809!>
2019-01-25 02:04:06 -0800

    The task for the editText field paired with the GetWebServiceTask is completed.

--- app/src/main/java/trungt/tcss450/uw/edu/webservicesandbox/MainActivity.java
+++ app/src/main/java/trungt/tcss450/uw/edu/webservicesandbox/MainActivity.java
@@ -1,5 +1,6 @@
 package trungt.tcss450.uw.edu.webservicesandbox;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -45,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
                 task.execute(getString(R.string.ep_hello));
                 break;
             case R.id.buttonHelloGet:
+                task = new GetWebServiceTask();
+                task.execute(getString(R.string.ep_base_url),
+                        getString(R.string.ep_hello_args),
+                        message);
                 break;
             case R.id.buttonHelloPost:
                 break;
@@ -95,5 +100,54 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private class GetWebServiceTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String...strings) {
+            if(strings.length != 3) {
+                throw new IllegalArgumentException("Three String arguments required.");
+            }
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            // Instead of using a hardcoded (found in end_points.xml) url for our webservice
+            // address, here we will build the URL from parts. This can be helpful when
+            // sending arguments via GET. In this example, we are sending plain text.
+            String url = strings[0];
+            String endPoint = strings[1];
+            String args = strings[2];
+            Uri uri = new Uri.Builder()
+                    .scheme("https")
+                    .appendPath(url)
+                    .appendPath(endPoint)
+                    .appendQueryParameter("name", args)
+                    .build();
+            try {
+                URL urlObject = new URL(uri.toString());
+                urlConnection = (HttpURLConnection) urlObject.openConnection();
+                InputStream content = urlConnection.getInputStream();
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                String s = "";
+                while ((s = buffer.readLine()) != null) {
+                    response += s;
+                }
+            } catch(Exception e) {
+                response = "Unable to connect, Reason:"
+                        + e.getMessage();
+            } finally {
+                if(urlConnection != null)
+                    urlConnection.disconnect();
+            }
 
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result.startsWith("Unable to")) {
+                // Something wrong. This is not the Best way to handle an error.
+                ((EditText) findViewById(R.id.inputEditText)).setError(result);
+            } else {
+                mTextView.setText(result);
+            }
+        }
+    }
 }
